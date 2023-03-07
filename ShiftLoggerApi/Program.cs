0c3d371b6539b100ce95c559d1d9b396ddb9e2be
Abubakar Ahmed <abubakarahmed4497@outlook.com>
2023-03-07 17:22:42 +0900

    swagger testing starts here

--- ShiftLoggerApi/Program.cs
+++ ShiftLoggerApi/Program.cs
@@ -1,8 +1,12 @@
+using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+var dbPath = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ShiftLogger.db");
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ShiftLoggerDbContext>(options => options.UseSqlite($"Data Source={dbPath}"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
